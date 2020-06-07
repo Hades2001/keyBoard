ed208c8abde7e20a22323bc178d55e098c1b0e40
Hades2001 <1521377294@qq.com>
2020-06-07 21:47:03 +0800

    add some files to project

---
+++ toolsbutton.cpp
@@ -0,0 +1,6 @@
+#include "toolsbutton.h"
+
+ToolsButton::ToolsButton()
+{
+
+}
---
+++ toolsbutton.h
@@ -0,0 +1,12 @@
+#ifndef TOOLSBUTTON_H
+#define TOOLSBUTTON_H
+
+#include <QWidget>
+
+class ToolsButton : public QPushButton
+{
+public:
+    ToolsButton();
+};
+
+#endif // TOOLSBUTTON_H
